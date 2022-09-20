26c4d4cf8a78dc01b3450ea84fbddcb34a31891e
Abhay999 <abhayjaat999@gmail.com>
2022-09-20 21:27:20 +0530

    biuty

--- src/routes/route.js
+++ src/routes/route.js
@@ -3,7 +3,9 @@ const router = express.Router()
 const userController= require("../controller/userController")
 
 router.post("/register",userController.createUser)
+
 router.post("/login",userController.loginUser)
+
 router.all("/*", (req, res) => { res.status(400).send({ status: false, message: "Endpoint is not correct" }) })
 
 module.exports = router;
--- src/validator/validator.js
+++ src/validator/validator.js
@@ -1,42 +1,39 @@
 const mongoose = require("mongoose");
 
 const isValidName = function (value) {
-  if (typeof value === 'undefined' || value === null)
-{return false;}
-  if (
-    typeof value === "string" &&
-    value.trim().length > 0 &&
-    /^[A-Z][a-z]*\s[A-Z][a-z]\D*$/.test(value)
-  )
+  if (typeof value === 'undefined' || value === null){return false;}
+  if ( typeof value === "string" && value.trim().length > 0 && /^[A-Z][a-z]*\s[A-Z][a-z]\D*$/.test(value))
     {return true;}
    return false;
 };
+
 const isValid = function (value) {
   if (typeof value === "string" && value.trim().length > 0) return true;
   return false;
 };
+
 const isValidLink = function (value) {
-  if (/^https?:\/\/.*\.[s3].*\.(png|gif|webp|jpeg|jpg)\??.*$/gim.test(value))
-    return true;
+  if (/^https?:\/\/.*\.[s3].*\.(png|gif|webp|jpeg|jpg)\??.*$/gim.test(value)) return true;
   return false;
 };
+
 const isValidMobile = function (value) {
   if (typeof value === "string" && /^[0-9]\d{9}$/gi.test(value)) return true;
   return false;
 };
+
 const isValidpin = function (value) {
   if (typeof value === "string" && /[0-9]\d{5}$/gi.test(value)) return true;
   return false;
 };
+
 const isValidEmail = function (value) {
   if (/^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$/.test(value)) return true;
   return false;
 };
 
 const isValidPassword = function (value) {
-  if ( typeof value === "string" &&
-  value.trim().length > 0 &&
-  /^[a-zA-Z0-9]{8,15}$/.test(value)) return true;
+  if ( typeof value === "string" && value.trim().length > 0 && /^[a-zA-Z0-9]{8,15}$/.test(value)) return true;
   return false;
 };
 
@@ -49,14 +46,5 @@ const isValidObjectId = function (objectId) {
 };
 
 
-module.exports = {
-  isValid,
-  isValidRequestBody,
-  isValidObjectId,
-  isValidEmail,
-  isValidPassword,
-  isValidName,
-  isValidMobile,
-  isValidLink,
-  isValidpin
-};
+module.exports = { isValid, isValidRequestBody, isValidObjectId, isValidEmail, isValidPassword,
+ isValidName, isValidMobile, isValidLink, isValidpin };

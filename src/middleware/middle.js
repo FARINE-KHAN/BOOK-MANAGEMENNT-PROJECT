55aa094667e59a310e49c928c571b8ae96b583ba
sumit <nsumit181993@gmail.com>
2022-09-20 17:07:24 +0530

    auth

---
+++ src/middleware/middle.js
@@ -0,0 +1,24 @@
+const jwt =require('jsonwebtoken')
+const authenticate = (req, res, next) => {
+    try {
+        let token = req.headers["x-auth-token"];
+        if (!token)
+            return res.status(401).send({ status: false, msg: "token is required" });
+        jwt.verify(token, "Project-3_Group-5", function (error, decoded) {
+            if (error) {
+                return res.status(401).send({ status: false, msg: "Authentication failed" });
+            } else {
+                req.token = decoded;
+                next();
+            }
+        });
+    } catch (error) {
+        res.status(500).send({ status: false, err: error.message });
+    }
+};
+
+//const authorisation=async (req,res,next)=>{
+    
+module.exports={authenticate,authorisation}
+
+

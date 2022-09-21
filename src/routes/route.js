fb71ae6e47287c78469aec7809b294e9aca5a5c3
alok <alokpatle123@gmail.com>
2022-09-21 19:50:56 +0530

    authorisation

--- src/routes/route.js
+++ src/routes/route.js
@@ -4,12 +4,20 @@ const userController= require("../controller/userController")
 const bookController= require("../controller/bookController")
 const middleware=require("../middleware/middle")
 
+
+// ============================================USER API======================================================
+
 router.post("/register",userController.createUser)
 router.post("/login",userController.loginUser)
 
-router.post("/books",bookController.createbook)
-router.get("/books",bookController.getBook)
-router.get("/books/:bookId",bookController.getById)
+// ==========================================================================================================
+
+// =============================================BOOK API==================================================
+
+router.post("/books",middleware.authentication,middleware.authorisation,bookController.createbook)
+router.get("/books",middleware.authentication,bookController.getBook)
+router.get("/books/:bookId",middleware.authentication,bookController.getById)
+
 
 router.all("/*", (req, res) => { res.status(400).send({ status: false, message: "Endpoint is not correct" }) })
 

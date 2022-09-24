72c6913e004088adfc1561523d1d929234b84c2b
alok <alokpatle123@gmail.com>
2022-09-24 18:10:47 +0530

    update

--- src/controller/reviewController.js
+++ src/controller/reviewController.js
@@ -18,6 +18,7 @@ const createReview = async function (req, res) {
         .status(400)
         .send({ status: false, msg: " valid book id is required" });
     }
+    
     let present = await bookModel.findById(bookId)
     if (!present) { return res.status(404).send({ status: false, msg: "Book not Available" }); }
     if (present.isDeleted === true) { return res.status(404).send({ status: false, msg: "Book is deleted" }); }
@@ -40,17 +41,20 @@ const createReview = async function (req, res) {
       }
     }
 
-   const book = await bookModel.findByIdUpdate(
+   const book = await bookModel.findOneAndUpdate(
       { bookId: bookId, isDeleted: false },
       { $inc: { reviews: 1 } }
     );
 
     reviewData.bookId = bookId;
+    
     reviewData.reviewedAt = moment();
     const createdReview = await reviewModel.create(reviewData);
+
+    const data= await reviewModel.findOne(reviewData).populate("bookId").select({updatedAt:0,createdAt:0,__v:0})
     res
       .status(201)
-      .send({ status: true, message: "success", data: [createdReview, book]  });
+      .send({ status: true, message: "success", data: data  });
   } catch (error) {
     res.status(500).send({ status: false, err: error.message });
   }
@@ -98,7 +102,9 @@ const updateReview = async (req, res) => {
       dataToBeUpdated._id,
       { reviewedBy: reviewedBy, rating: rating, review: review },
       { new: true }
-    );
+    ).populate("bookId");
+    
+
     return res.status(200).send({ msg: "done", data: updatedData });
   } catch (error) {
     res.status(500).send({ status: false, message: err.message });
